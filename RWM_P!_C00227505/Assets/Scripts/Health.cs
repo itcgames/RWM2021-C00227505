063d868b2036dede77746a1ec310a0a7b345c9ea
MohammadAlhourani <34336016+MohammadAlhourani@users.noreply.github.com>
2020-11-12 14:05:52 +0000

    added player health ui

---
+++ RWM_P!_C00227505/Assets/Scripts/Health.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public int health;
+
+    public int numOfHearts;
+
+    public Image[] hearts;
+    public Sprite FullHeart;
+
+
+    void Update()
+    {
+        for (int i = 0; i < hearts.Length; i++)
+        {
+
+            if(health > numOfHearts)
+            {
+                health = numOfHearts;
+            }
+
+
+            if(i < health)
+            {
+                hearts[i].enabled = true;
+                hearts[i].sprite = FullHeart;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+
+
+            //if(i < numOfHearts)
+            //{
+            //    hearts[i].enabled = true;
+            //}
+            //else
+            //{
+            //    hearts[i].enabled = false;
+            //}
+        }
+    }
+}
