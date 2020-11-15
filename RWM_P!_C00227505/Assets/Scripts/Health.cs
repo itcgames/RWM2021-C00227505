a970aa16c7a131864dacb8ad159563cb473986b2
MohammadAlhourani <34336016+MohammadAlhourani@users.noreply.github.com>
2020-11-15 14:40:59 +0000

    added empty heart and ability to scale to players health changes

--- RWM_P!_C00227505/Assets/Scripts/Health.cs
+++ RWM_P!_C00227505/Assets/Scripts/Health.cs
@@ -9,39 +9,25 @@ public class Health : MonoBehaviour
     public int health;
 
     public int numOfHearts;
-
-    public GameObject[,] hearts;
+      
 
     void Update()
     {
-        for (int i = 0; i < hearts.Length; i++)
+       
+        if(health > numOfHearts)
         {
-
-            if(health > numOfHearts)
-            {
-                health = numOfHearts;
-            }
-
-
-            if(i < health)
-            {
-                //hearts[i].enabled = true;
-                //hearts[i].sprite = FullHeart;
-            }
-            else
-            {
-                //hearts[i].enabled = false;
-            }
-
-
-            //if(i < numOfHearts)
-            //{
-            //    hearts[i].enabled = true;
-            //}
-            //else
-            //{
-            //    hearts[i].enabled = false;
-            //}
+            health = numOfHearts;
         }
+           
+    }
+
+    void takeDmg()
+    {
+        health--;
+    }
+
+    void heal()
+    {
+        health++;
     }
 }
--- RWM_P!_C00227505/Assets/Scripts/Hearts.cs
+++ RWM_P!_C00227505/Assets/Scripts/Hearts.cs
@@ -5,6 +5,7 @@ using System.Collections.Specialized;
 using System.Globalization;
 using System.Security.Cryptography;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Hearts : MonoBehaviour
 {
@@ -12,9 +13,15 @@ public class Hearts : MonoBehaviour
     public GameObject Player;
 
     public GameObject sprite;
-    private int numOfHearts;
-    public int numOfRows;
 
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+    //players max Health
+    private int numOfHearts;
+
+
+    public int numOfRows;
     private int numOfCol;
 
     public float spriteWidth;
@@ -26,16 +33,50 @@ public class Hearts : MonoBehaviour
 
     private void Start()
     {
-
-        int num = 0;
+               
 
         numOfHearts = Player.GetComponent<Health>().numOfHearts;
 
+
+
+        createHearts();
+       
+
+     
+    }
+
+    private void Update()
+    {
+        int num = 0;
+
+        for (int i = 0; i < numOfRows; i++)
+        {
+            for (int y = 0; y < numOfCol; y++)
+            {
+                if (num < Player.GetComponent<Health>().health && num < numOfHearts)
+                {
+                    hearts[i, y].GetComponent<Image>().sprite = fullHeart;
+                    num++;
+                }
+                else if (num < numOfHearts)
+                {
+                    hearts[i, y].GetComponent<Image>().sprite = emptyHeart;
+                    num++;
+                }
+
+            }
+        }
+    }
+
+
+    private void createHearts()
+    {
+        int num = 0;
+
         numOfCol = Mathf.CeilToInt(((float)numOfHearts / (float)numOfRows));
 
-        hearts = new GameObject[numOfRows,numOfCol];
+        hearts = new GameObject[numOfRows, numOfCol];
 
-       
         for (int i = 0; i < numOfRows; i++)
         {
             for (int y = 0; y < numOfCol; y++)
@@ -45,16 +86,9 @@ public class Hearts : MonoBehaviour
                     hearts[i, y] = Instantiate(sprite, transform.position + (new Vector3(spriteWidth * y, spriteHeight * -i, 0)), Quaternion.identity, transform);
                     num++;
                 }
-                
+
             }
         }
-
-     
-    }
-
-    private void Update()
-    {
-        Player.GetComponent<Health>().hearts = this.hearts;
     }
 
 }
