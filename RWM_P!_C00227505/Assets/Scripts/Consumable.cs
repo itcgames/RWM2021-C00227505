3243f9a29159f186db38c7ddcdfd520e7016d244
MohammadAlhourani <34336016+MohammadAlhourani@users.noreply.github.com>
2020-11-20 17:00:08 +0000

    added basic consumable functionality

---
+++ RWM_P!_C00227505/Assets/Scripts/Consumable.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Consumable : MonoBehaviour
+{
+    public GameObject Player;
+
+    public GameObject ConsumablePrefab;
+
+    public int amount;
+
+    private Text amountText;
+
+    private Font arial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+   
+        arial = (Font)Resources.GetBuiltinResource(typeof(Font), "Arial.ttf");
+
+        gameObject.AddComponent<Text>();
+        amountText = gameObject.GetComponent<Text>();
+        amountText.font = arial;
+        amountText.text = "X" + amount.ToString() ;
+        amountText.fontSize = 48;
+        //amountText.GetComponent<RectTransform>().localPosition += new Vector3(40, 0, 0);
+
+        Instantiate(ConsumablePrefab, transform.position - new Vector3(60, -20,0), Quaternion.identity, transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        amountText.text = "X" + amount.ToString();
+    }
+
+    public void use()
+    {
+        //insert use code here
+        //e.g
+        Player.GetComponent<Health>().heal();
+    }
+}
