0a6bdbb79445734883ccec0607b7047acb7c6402
MohammadAlhourani <34336016+MohammadAlhourani@users.noreply.github.com>
2020-11-23 16:29:03 +0000

    added health potion functionality

--- RWM_P!_C00227505/Assets/Scripts/Consumable.cs
+++ RWM_P!_C00227505/Assets/Scripts/Consumable.cs
@@ -25,22 +25,57 @@ public class Consumable : MonoBehaviour
         amountText = gameObject.GetComponent<Text>();
         amountText.font = arial;
         amountText.text = "X" + amount.ToString() ;
-        amountText.fontSize = 48;
-        //amountText.GetComponent<RectTransform>().localPosition += new Vector3(40, 0, 0);
+        amountText.fontSize = 24;
+       
 
-        Instantiate(ConsumablePrefab, transform.position - new Vector3(60, -20,0), Quaternion.identity, transform);
+        float Width = ConsumablePrefab.GetComponent<RectTransform>().rect.width;
+
+        float height = ConsumablePrefab.GetComponent<RectTransform>().rect.height * 0.60f;
+
+        Debug.Log(Width + " " + height);
+
+        Instantiate(ConsumablePrefab, transform.position - new Vector3(Width, -height,0), Quaternion.identity, transform);
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
         amountText.text = "X" + amount.ToString();
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            use();
+        }
+    }
+
+    public void AddAmount()
+    {
+        amount++;
     }
 
     public void use()
     {
-        //insert use code here
-        //e.g
-        Player.GetComponent<Health>().heal();
+        if (ConsumablePrefab.GetComponent<HealthPotion>() != null)
+        {
+            ConsumablePrefab.GetComponent<HealthPotion>().setPlayer(Player);
+
+            
+        }
+
+        if (amount > 0)
+        {
+            if (ConsumablePrefab.GetComponent<HealthPotion>() != null)
+            {
+                if (Player.GetComponent<Health>().health < Player.GetComponent<Health>().numOfHearts)
+                {
+                    ConsumablePrefab.GetComponent<HealthPotion>().use();
+
+                    amount--;
+                }
+            }
+        }
     }
 }
---
+++ RWM_P!_C00227505/Assets/Scripts/HealthPotion.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPotion : MonoBehaviour
+{
+    public GameObject Player;
+
+
+    public void setPlayer(GameObject t_Player)
+    {
+        Player = t_Player;
+
+        
+    }
+
+    public void use()
+    {
+        Player.GetComponent<Health>().heal();
+    }
+}
